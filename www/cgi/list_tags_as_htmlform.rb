0b15873a0eba5d344a6cf547d219c088611c37d0
Bernd Brägelmann <bernd@braegelmann.de>
2011-07-07 21:07:44 +0200

    prepared file for html generation

---
+++ www/cgi/list_tags_as_htmlform.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require '../lib/Diagnose.rb'
+require '../lib/Tags.rb'
+
+def collect_tags(diagnosesis)
+  tags=Tags.new
+  
+  diagnosesis.each do |diagnose|
+
+    diagnose.xray.each do |tag|
+      tags.xray[tag]= tags.xray[tag]+1
+    end
+    diagnose.ct.each do |tag|
+      tags.ct[tag]= tags.ct[tag]+1
+    end
+    diagnose.mri.each do |tag|
+      tags.mri[tag]= tags.mri[tag]+1
+    end
+
+  end
+  
+  tags
+end
+
+diagnosesis = read_diagnozo
+
+usedtags=Tags.new
+usedtags=collect_tags(diagnosesis)
+#usedtags.sort
+puts "All used tags"
+usedtags.print
