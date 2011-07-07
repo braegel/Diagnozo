b55dc00112f930a9bb3845d11426b933037e2df9
Bernd Braegelmann <bernd@braegelmann.de>
2011-07-07 15:27:56 +0200

    added def read_diagnozo into class file (not into the class) is that convenient?

--- lib/Diagnose.rb
+++ lib/Diagnose.rb
@@ -1,3 +1,42 @@
+require 'xmlsimple'
+
+def read_diagnozo
+  diagnozo = XmlSimple.xml_in('../data/diagnozo.xml')
+
+  diagnosis = Array.new
+
+  diagnozo['diagnosis'].each do |diagnose|
+    adiagnose=Diagnose.new(diagnose['name'])
+
+    if diagnose['url']
+      diagnose['url'].each { |url| adiagnose.url << url}
+    end
+
+    if diagnose['region']
+      diagnose['region'].each { |region| adiagnose.region << region}
+    end
+
+    if diagnose['xray']
+      diagnose['xray'].each { |xray| adiagnose.xray << xray}
+    end
+
+    if diagnose['ct']
+      diagnose['ct'].each { |ct| adiagnose.ct << ct}
+    end
+
+    if diagnose['mri']
+      diagnose['mri'].each { |mri| adiagnose.mri << mri}
+    end
+
+    if diagnose['dd']
+      diagnose['dd'].each { |dd| adiagnose.dd << dd}
+    end
+
+    diagnosis << adiagnose
+  end
+  return diagnosis
+end
+
 class Diagnose
   attr_reader :name
   attr_accessor :url,:region,:xray,:mri, :ct, :dd
@@ -30,7 +69,6 @@ class Diagnose
       @mri.each do |url|
         puts "\tmri: #{url}"
       end
-
     puts
   end
   
--- tools/show_diagnosis.rb
+++ tools/show_diagnosis.rb
@@ -1,39 +1,6 @@
-require 'xmlsimple'
 require '../lib/Diagnose.rb'
-diagnozo = XmlSimple.xml_in('../data/diagnozo.xml')
 
-diagnosis = Array.new
-
-diagnozo['diagnosis'].each do |diagnose|
-  adiagnose=Diagnose.new(diagnose['name'])
-
-  if diagnose['url']
-    diagnose['url'].each { |url| adiagnose.url << url}
-  end
-    
-  if diagnose['region']
-    diagnose['region'].each { |region| adiagnose.region << region}
-  end
-
-  if diagnose['xray']
-    diagnose['xray'].each { |xray| adiagnose.xray << xray}
-  end
-  
-  if diagnose['ct']
-    diagnose['ct'].each { |ct| adiagnose.ct << ct}
-  end
-
-  if diagnose['mri']
-    diagnose['mri'].each { |mri| adiagnose.mri << mri}
-  end
-  
-  if diagnose['dd']
-    diagnose['dd'].each { |dd| adiagnose.dd << dd}
-  end
-  
-  diagnosis << adiagnose
-end
-
+diagnosis = read_diagnozo
 
 diagnosis.each do |diagnose| 
   diagnose.print
