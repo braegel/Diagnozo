acd5b45149ab2230eb021bad3e37626574e6a4f6
Bernd Braegelmann <bernd@braegelmann.de>
2011-07-13 07:06:06 +0200

    xml grabbing is working

--- js/diagnozo.js
+++ js/diagnozo.js
@@ -1,3 +1,37 @@
-console.log(undefined == null)
-   
-console.log(undefined === null)
+var xmlhttp=false;
+/*@cc_on @*/
+/*@if (@_jscript_version >= 5)
+// JScript gives us Conditional compilation, we can cope with old IE versions.
+// and security blocked creation of the objects.
+ try {
+  xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+ } catch (e) {
+  try {
+   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (E) {
+   xmlhttp = false;
+  }
+ }
+ @end @*/
+if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+    try {
+	xmlhttp = new XMLHttpRequest();
+    } catch (e) {
+	xmlhttp=false;
+    }
+}
+if (!xmlhttp && window.createRequest) {
+    try {
+	xmlhttp = window.createRequest();
+    } catch (e) {
+	xmlhttp=false;
+    }
+}
+
+xmlhttp.open("GET", "../data/diagnozo.xml",true);
+xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4) {
+		document.write(xmlhttp.responseText)
+    }
+}
+    xmlhttp.send(null)
